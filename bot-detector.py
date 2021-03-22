17dcaa8abbbcfce24fa5bc0a44d8d231609866ed
krishna453 <79736244+krishna453@users.noreply.github.com>
2021-03-22 11:18:51 +0530

    Update bot-detector.py

--- bot-detector.py
+++ bot-detector.py
@@ -4,8 +4,8 @@ import threading
 import heroku
 Form time import slep
 Client=Amino.client()
-email='mouryaravi55@gmail.com'
-Posswod='ravikrishnaoppo'
+email='ravimourya526@yahoo.com'
+Posswod='ravi1234oppo'
 Client.login(email=email, posswod=posswod)
 Comid=[17015136,14999388] # indian time , indians vip
 Mtyp=[100,107,109,110,114,115]
