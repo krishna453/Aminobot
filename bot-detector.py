682b45b854c6fd72cdd16b3d457254153f826e9e
krishna453 <79736244+krishna453@users.noreply.github.com>
2021-04-13 21:02:16 +0530

    Update bot-detector.py

--- bot-detector.py
+++ bot-detector.py
@@ -1,22 +1,7 @@
-import Amino
-import threading
-import heroku
-Form time import slep
-Client=Amino.client()
-email='ravimourya20@yahoo.com'
-Posswod='ravioppo1234'
-Client.login(email=email, posswod=posswod)
-Comid=[17015136,14999388] # indian time , indians vip
-Mtyp=[100,107,109,110,114,115]
-Me['e85600276-7896-41ca-964e-3aa7358b
-
-Heroku_conn= heroku.from_key('2510e
-Botapps= heroku_conn.apps()[' botdetect
-Welcome message= [.setw]
-
-
-    Def search_bot(message,comid):
-       if message.json['type'] in mtype
-           if client.get_user_info(message author.
-               Subclient=Amino.subclient(comid=c
+import amino
+client = amino.Client()
+client.login(email='botamino016@fake-box.com',password='123ironman')
+comlist= client.sub_clients(size=100)
+for name, id in zip(comlist.name,comlist.comId):
+    print("Comm Name  : ",name,"\nComm Id :",id,"\n--------------")
 © 2021 GitHub, Inc.
