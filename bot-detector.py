ca3b8dd5694466c97d410911f3562f29b1212c53
krishna453 <79736244+krishna453@users.noreply.github.com>
2021-04-17 00:16:18 +0530

    Update bot-detector.py

--- bot-detector.py
+++ bot-detector.py
@@ -3,5 +3,5 @@ client = amino.Client()
 client.login(email='botamino016@fake-box.com',password='123ironman')
 comlist= client.sub_clients(size=100)
 for name, id in zip(comlist.name,comlist.comId):
-    print("Comm Name  : ",name,"\nComm Id :",id,"\n--------------")
+    print("Comm Name  : ", Indians vip,"\nComm Id :",14999388,"\n--------------")
 © 2021 GitHub, Inc.
