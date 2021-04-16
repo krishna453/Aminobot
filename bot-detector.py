c4fde216b146f40ce788efba2c321eeff0ad103b
krishna453 <79736244+krishna453@users.noreply.github.com>
2021-04-17 00:17:54 +0530

    Create bot-detector.py

--- bot-detector.py
+++ bot-detector.py
@@ -1,6 +1,6 @@
 import amino
 client = amino.Client()
-client.login(email='botamino016@fake-box.com',password='123ironman')
+client.login(email='ravimourya20@yahoo.com',password='ravioppo1234')
 comlist= client.sub_clients(size=100)
 for name, id in zip(comlist.name,comlist.comId):
     print("Comm Name  : ", Indians vip,"\nComm Id :",14999388,"\n--------------")
